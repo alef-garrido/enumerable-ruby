d95b21c4bd8705dc6ed97ea063ab547b9e9e8c3e
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-05-20 18:39:57 +0200

    Fix the linter errors

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,10 +1,17 @@
-class Mylist
-  attr_accessor :list  def initialize(*list)
-    @list = list
+module MyEnumerable
+  def all?
+    @list.each { |item| return p false unless yield item }
+    p true
   end
-  include MyEnumerable  def each
-    @list.each { |item| yield item if block_given? }
+
+  def any?
+    @list.each { |item| return p true if yield item }
+    p false
   end
-endlist = Mylist.new(1, 2, 3, 4)list.all? { |e| e < 5 }
-list.all? { |e| e > 5 }list.any? { |e| e == 2 }
-list.any? { |e| e == 5 }list.filter(&:even?)
+
+  def filter
+    arr = []
+    @list.each { |item| arr << item if yield item }
+    p arr
+  end
+end
--- my_list.rb
+++ my_list.rb
@@ -1,4 +1,5 @@
 require './my_enumerable'
+
 class Mylist
   attr_accessor :list
 
@@ -6,13 +7,18 @@ class Mylist
     @list = list
   end
   include MyEnumerable
+
   def each
     @list.each { |item| yield item if block_given? }
   end
 end
+
 list = Mylist.new(1, 2, 3, 4)
+
 list.all? { |e| e < 5 }
 list.all? { |e| e > 5 }
+
 list.any? { |e| e == 2 }
 list.any? { |e| e == 5 }
+
 list.filter(&:even?)
